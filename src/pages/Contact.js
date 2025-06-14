a35a96e819cc0fa4802082fb1498e4f443754e10
vthirtythr33 <aaronhui88@gmail.com>
2025-06-14 01:32:01 -0400

    added forms to Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,8 +7,12 @@ function Contact() {
         <div className='contactContent'>
           <h1 className='contactTitle'>Contact Us</h1>
           <p className='contactInfo'>
-              violetdiabolo@gmail.com
+              Reach out to <a href='mailto:violetdiabolo@gmail.com'>violetdiabolo@gmail.com</a> to contact us!
           </p>
+          <div>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqk-vvGryB5SCO2AM-iL2FXDi_2MNJHLJxnyxeckUNRoRzgw/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdGVUpii4Viiv3EPtoDtXCyk9l7dxhbi3pINhJiN_KLiIwT4g/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>          
+          </div>
         </div>
     </div>
   )
