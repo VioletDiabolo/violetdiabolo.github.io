c22941fac2399984075a6ebd16c461bd8cbf084a
vthirtythr33 <aaronhui88@gmail.com>
2025-06-23 21:51:32 -0400

    big changes to homepage, some small css fixes elsewhere

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,21 @@ import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home' style={{ backgroundImage: `url(${BannerImage})` }}>
-      <div className='headerContainer'>
-        <h1>VIOLET DIABOLO</h1>
-        <p>PREMIER DIABOLO TEAM AT NYU</p>
-      </div>
+    <div className='home'>
+        <div className='homeTop' style={{ backgroundImage: `url(${BannerImage})` }}></div>
+        <div className='homeBottom'>
+          <div className='homeContent'>
+            <h1 className='name'>VIOLET DIABOLO</h1>
+            <p className='description'>PREMIER DIABOLO TEAM AT NYU</p>
+          </div>
+        </div>
+        <div className='homeFormsContent'>
+          <h1>FORMS</h1>
+          <div className='homeForms'>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdqk-vvGryB5SCO2AM-iL2FXDi_2MNJHLJxnyxeckUNRoRzgw/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdGVUpii4Viiv3EPtoDtXCyk9l7dxhbi3pINhJiN_KLiIwT4g/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>          
+          </div>
+        </div>
     </div>
   )
 }
