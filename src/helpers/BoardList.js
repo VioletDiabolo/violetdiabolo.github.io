5bd7646172b2755794ab5edbc30cacdf9f067ce0
vthirtythr33 <aaronhui88@gmail.com>
2025-06-24 19:32:07 -0400

    reworking board page with dropdowns

--- src/helpers/BoardList.js
+++ src/helpers/BoardList.js
@@ -1,20 +1,96 @@
-export const BoardList = [
-    {
-        image: 'https://violetdiabolo.github.io/aaron_boardphoto.jpg',
-        name: 'Aaron Hui',
-        position: 'President',
-        description: "Heyo, I'm Aaron and I'm the current president of Violet Diabolo! I am a vertax one-trick (which means that you should be very careful near me when I'm yoyoing), but I'm currently working on 3D and trying to learn more integrals! In my free time I like to play Tetris (modern, not NES) and spin other non yoyo props like poi, whip, staff, or ropedart. Sometimes you'll catch me playing with fire :)"
-    },
-    {
-        image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
-        name: 'Jonathan Sun',
-        position: 'Artistic Director',
-        description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
-    },
-    {
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
-        name: 'N/A',
-        position: 'N/A',
-        description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
-    }
-]
+export const BoardList = {
+    'Fall 2025': [
+        {
+            image: 'https://violetdiabolo.github.io/aaron_boardphoto.jpg',
+            name: 'Aaron Hui',
+            position: 'President',
+            description: "Heyo, I'm Aaron and I'm the current president of Violet Diabolo! I am a vertax one-trick (which means that you should be very careful near me when I'm yoyoing), but I'm currently working on 3D and trying to learn more integrals! In my free time I like to play Tetris (modern, not NES) and spin other non yoyo props like poi, whip, staff, or ropedart. Sometimes you'll catch me playing with fire :)"
+        },
+        {
+            image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
+            name: 'Jonathan Sun',
+            position: 'Artistic Director',
+            description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
+        },
+        {
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
+            name: 'N/A',
+            position: 'N/A',
+            description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
+        }
+    ],
+    'Spring 2025': [
+        {
+            image: 'https://violetdiabolo.github.io/aaron_boardphoto.jpg',
+            name: 'Aaron Hui',
+            position: 'President',
+            description: "Heyo, I'm Aaron and I'm the current president of Violet Diabolo! I am a vertax one-trick (which means that you should be very careful near me when I'm yoyoing), but I'm currently working on 3D and trying to learn more integrals! In my free time I like to play Tetris (modern, not NES) and spin other non yoyo props like poi, whip, staff, or ropedart. Sometimes you'll catch me playing with fire :)"
+        },
+        {
+            image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
+            name: 'Jonathan Sun',
+            position: 'Artistic Director',
+            description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
+        },
+        {
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
+            name: 'N/A',
+            position: 'N/A',
+            description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
+        }
+    ],
+    'Fall 2024': [
+        {
+            image: 'https://violetdiabolo.github.io/aaron_boardphoto.jpg',
+            name: 'Aaron Hui',
+            position: 'President',
+            description: "Heyo, I'm Aaron and I'm the current president of Violet Diabolo! I am a vertax one-trick (which means that you should be very careful near me when I'm yoyoing), but I'm currently working on 3D and trying to learn more integrals! In my free time I like to play Tetris (modern, not NES) and spin other non yoyo props like poi, whip, staff, or ropedart. Sometimes you'll catch me playing with fire :)"
+        },
+        {
+            image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
+            name: 'Jonathan Sun',
+            position: 'Artistic Director',
+            description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
+        },
+        {
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
+            name: 'N/A',
+            position: 'N/A',
+            description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
+        }
+    ],
+    'Spring 2024': [
+        {
+            image: 'https://violetdiabolo.github.io/aaron_boardphoto.jpg',
+            name: 'Aaron Hui',
+            position: 'Secretary',
+            description: "This website was created a year after this time period, so I guess I can't really write that I'm the current president, but presumably I was sending a lot of practice emails and grinding vertax and 2D around this time haha. Probably destroying my ribs with body hit gens or failing to learn heli..."
+        },
+        {
+            image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
+            name: 'Jonathan Sun',
+            position: 'Artistic Director',
+            description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
+        },
+        {
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
+            name: 'N/A',
+            position: 'N/A',
+            description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
+        }
+    ],
+    'Fall 2023': [
+        {
+            image: 'https://violetdiabolo.github.io/jon_boardphoto.jpg',
+            name: 'Jonathan Sun',
+            position: 'Artistic Director',
+            description: "Yo. I'm JonaSun. Resident transplant from UMich Revolution. I’m all about carefully crafting combos for creative, yet chaotic, choreography. Proud proponent of plasma torch for cutting yo-yo string. I'm currently working on getting DNA into flare (a.k.a. cancer) consistently. When I'm not spinning, I am mad scientist. It is so cool!"
+        },
+        {
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM2OJy9rNds8J8DrtvjImMk-krlUMtDd0hfjdEwQORF9HKPxxSNClCYBCRxMi4dZS-0c&usqp=CAU',
+            name: 'N/A',
+            position: 'N/A',
+            description: 'More board members coming soon! People have not sent me photos and blurbs yet :('
+        }
+    ]
+}
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BoardList } from '../helpers/BoardList'
 import BoardItem from '../components/BoardItem'
 import "../styles/Board.css"
 
 function Board() {
+  const semesters = Object.keys(BoardList)
+  const [selectedSemester, setSelectedSemester] = useState(semesters[0])
   return (
     <div className='board'>
       <div className='boardContent'>
         <h1 className='boardTitle'>Board</h1>
+        <select 
+          className='semesterDropdown'
+          value={selectedSemester} 
+          onChange={(e) => setSelectedSemester(e.target.value)}
+        >
+          {semesters.map((semester, index) => (
+            <option key={index} value={semester}>{semester}</option>
+          ))}
+        </select>
         <div className='boardList'>
-            {BoardList.map((boardItem, key) => {
+            {BoardList[selectedSemester].map((boardItem, key) => {
             return (
                 <BoardItem
                 key={key}
